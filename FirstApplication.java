a1f1a78731a2210d2d7c7f885d4bf5ab1ba5b7b0
Vimoksh Bhavsar <85449611+tankotkirn@users.noreply.github.com>
2023-06-19 15:05:12 +0530

    SpringBoot Hello World

---
+++ FirstApplication.java
@@ -0,0 +1,15 @@
+package com.vimoksh.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApplication.class, args);
+
+		System.out.println("Welcome to Boot");
+	}
+
+}
